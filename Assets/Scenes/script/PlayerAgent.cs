2f52ae44a9d40ae35ef0ccc8f2b18dfe04e4bce3
Tom Peeters <tom_ptrs@yahoo.com>
2020-09-28 19:54:11 +0200

    ml agents - first try

---
+++ Assets/Scenes/script/PlayerAgent.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class PlayerAgent : Agent
+{
+    public Transform Target;
+    Rigidbody rBody;
+    RayPerceptionSensorComponent3D rayPerception;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        rayPerception = GetComponent<RayPerceptionSensorComponent3D>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+            this.transform.rotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+        }
+
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(Random.Range(-4,4),
+                                           0.5f,
+                                           Random.Range(-4, 4));
+
+      
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+        sensor.AddObservation(rayPerception);
+
+    }   
+
+
+
+    public float forceMultiplier = 20;
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1]; 
+        transform.rotation = Quaternion.Euler(0f, vectorAction[2] * 20, 0f);       
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.4f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
